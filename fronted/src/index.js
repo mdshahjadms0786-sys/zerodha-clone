6751369a797c8f3974c274c5d8da81713f6e3e2c
mdshahjadms0786@gmail.com <mdshahjadms0786@gmail.com>
2026-06-30 20:42:25 +0530

    Updated Stats, Pricing, Navbar and other components

--- fronted/src/index.js
+++ fronted/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-
+import HomePage from './landing_page/home/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div >Hello Md</div>
+      <HomePage />
   </React.StrictMode>
 );
 
